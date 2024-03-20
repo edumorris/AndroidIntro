45738306478943625aae5e4a0da967ee7669957b
edumorris <eduuwachira@gmail.com>
2024-03-20 15:24:05 +0300

    keeping textview content on rotation

--- app/src/main/java/com/edumacation/android/MainActivity.java
+++ app/src/main/java/com/edumacation/android/MainActivity.java
@@ -1,9 +1,11 @@
 package com.edumacation.android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,9 +16,44 @@ public class MainActivity extends AppCompatActivity {
     private EditText userInputText;
     private TextView txtView;
     private int numOfTimesClicked = 0;
+    private static final String TAG = "MainActivity";
+    private final String TEXT_CONTENTS = "TextContents";
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: in");
+        super.onStart();
+        Log.d(TAG, "onStart: out");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
+        super.onRestoreInstanceState(savedInstanceState);
+//        String savedString = savedInstanceState.getString(TEXT_CONTENTS);
+//        txtView.setText(savedString);
+        txtView.setText(savedInstanceState.getString(TEXT_CONTENTS));
+        Log.d(TAG, "onRestoreInstanceState: out");
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart: in");
+        super.onRestart();
+        Log.d(TAG, "onRestart: out");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: in");
+        outState.putString(TEXT_CONTENTS, txtView.getText().toString()); // Needs to be before the super() method
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: out");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.button_click_counter);
 
@@ -38,5 +75,10 @@ public class MainActivity extends AppCompatActivity {
         };
 
         btn.setOnClickListener(ourOnClickListener);
+        Log.d(TAG, "onCreate: out");
+
+
     }
+
+
 }
